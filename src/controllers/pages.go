1877ab5e980b57e418e38cbf7986d0114680196b
thiago tanaka <thiago.tanaka.dev@gmail.com>
2023-02-09 23:20:43 +0900

    change home page

--- src/controllers/pages.go
+++ src/controllers/pages.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/models"
 	"webapp/src/requests"
+	"webapp/src/responses"
 	"webapp/src/utils"
 )
 
@@ -18,10 +21,32 @@ func LoadCreateAccountPage(w http.ResponseWriter, r *http.Request) {
 
 func LoadHomePage(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/posts", config.APIURL)
+	response, err := requests.MakeRequestWithAuth(r, http.MethodGet, url, nil)
 
-	response, _ := requests.MakeRequestWithAuth(r, http.MethodGet, url, nil)
+	if err != nil {
+		responses.JSON(w, http.StatusInternalServerError, responses.ApiError{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		responses.HandleError(w, response)
+		return
+	}
 
 	fmt.Println(response.StatusCode)
 
-	utils.ExecuteTemplate(w, "home.html", nil)
+	var posts []models.Post
+
+	if err = json.NewDecoder(response.Body).Decode(&posts); err != nil {
+		responses.JSON(w, http.StatusInternalServerError, responses.ApiError{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	utils.ExecuteTemplate(w, "home.html", posts)
 }
